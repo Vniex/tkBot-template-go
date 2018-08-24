6057d8737322b1ed94739e55fb9ffb88404c31b2
周斌 <zhoubin@ZB-MBP.local>
2018-08-24 20:31:20 +0800

    add robot control

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 
+	"tkBot-template-go/strategy"
+	"flag"
+	"log"
+	"encoding/json"
 	"tkBot-template-go/template"
 )
 
@@ -9,6 +13,25 @@ import (
 
 
 func main(){
-	robotHub:=template.NewRobotHub(template.RobotHubName,template.WebsocketServer,template.Heartbeat_Interval)
-	robotHub.Start()
+
+	mode := flag.String("mode", "robotHub", "start robotHub or robot, robotHub/robot")
+	parameter := flag.String("para", "{}", "robot parameterm json string")
+	flag.Parse()
+
+	var para strategy.Parameters
+	if *mode=="robotHub"{
+		robotHub:=template.NewRobotHub(strategy.RobotHubName,strategy.WebsocketServer,strategy.Heartbeat_Interval)
+		robotHub.Start()
+	}else if *mode=="robot"{
+		json.Unmarshal([]byte(*parameter),&para)
+		log.Println(*parameter)
+		log.Println(para)
+		robot:=strategy.NewRobot(&para)
+		robot.Start()
+
+	}else{
+		panic("no this mode")
+	}
+
 }
+
